ea3620faa9f3e8cf483d07ad08c8d5c7f66c97ea
Jason Rogena <jasonrogena@gmail.com>
2018-02-10 18:39:49 +0300

    Fix Loop Error in monitored-apps.sh
    Fix a syntax error in the for loop in monitored-apps.sh.
    
    Signed-off-by: Jason Rogena <jasonrogena@gmail.com>

--- templates/usr/lib/collectd/plugins/monitored-apps.sh
+++ templates/usr/lib/collectd/plugins/monitored-apps.sh
@@ -5,7 +5,7 @@ interval="${COLLECTD_INTERVAL:-60}"
 while sleep "$interval"
 do
   source {{ monitored_apps_env_dir }}/{{ monitored_apps_env_file }}
-  for (currentApp in "${{ '{' }}{{ monitored_apps_env_var }}[@]{{ '}' }}")
+  for currentApp in "${{ '{' }}{{ monitored_apps_env_var }}[@]{{ '}' }}"
   do
     name=${currentApp%;*}
     version=${currentApp#*;}
